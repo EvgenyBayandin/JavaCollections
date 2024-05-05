1a1b1873f86cbb8a1920ead57b072b71cce52fcc
Evgeny Bayandin <5087081@gmail.com>
2024-05-05 15:32:53 +0300

    Added Map

--- src/main/java/ru/webdev/CollectionsExample.java
+++ src/main/java/ru/webdev/CollectionsExample.java
@@ -5,8 +5,11 @@ package ru.webdev;
 //import java.util.LinkedList;
 //import java.util.List;
 //import java.util.LinkedHashSet;
-import java.util.Set;
-import java.util.TreeSet;
+//import java.util.Set;
+//import java.util.TreeSet;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class CollectionsExample {
     public static void main(String[] args) {
@@ -46,15 +49,22 @@ public class CollectionsExample {
 //
 //        System.out.println(fruits); // [apple, kiwi, banana, orange]
 
-        Set<String> fruits = new TreeSet<>(); // Based on Set, unique items, sorted by insertion
-        fruits.add("apple");
-        fruits.add("kiwi");
-        fruits.add("banana");
-        fruits.add("orange");
-        fruits.add("apple"); // Duplicate items will not be added
+//        Set<String> fruits = new TreeSet<>(); // Based on Set, unique items, sorted by insertion
+//        fruits.add("apple");
+//        fruits.add("kiwi");
+//        fruits.add("banana");
+//        fruits.add("orange");
+//        fruits.add("apple"); // Duplicate items will not be added
+//
+//        System.out.println(fruits); // [apple, banana, kiwi, orange]
 
-        System.out.println(fruits); // [apple, banana, kiwi, orange]
+        Map<String, Integer> fruits = new HashMap<>();
+        fruits.put("яблоко", 6); // дубликат перезаписывается новым значением ("яблоко", 4)
+        fruits.put("ананас", 6);
+        fruits.put("банан", 5);
+        fruits.put("яблоко", 4);
+        fruits.putIfAbsent("банан", 5); // здесь не добавится, т.к. такое значение уже есть, иначе добавится новое значение ("банан", 5)
 
-
+        System.out.println(fruits); // {банан=5, яблоко=4, ананас=6}
     }
 }
