99f49ee97fbf9eeb12c080de65764716626c2c6f
Evgeny Bayandin <5087081@gmail.com>
2024-05-05 23:13:16 +0300

    Added SequencedSet as TreeSet

--- src/main/java/ru/webdev/SequencedCollectionAndSetExample.java
+++ src/main/java/ru/webdev/SequencedCollectionAndSetExample.java
@@ -5,6 +5,7 @@ import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.SequencedCollection;
 import java.util.SequencedSet;
+import java.util.TreeSet;
 
 public class SequencedCollectionAndSetExample {
 
@@ -55,19 +56,26 @@ public class SequencedCollectionAndSetExample {
 //        long totalTime = System.currentTimeMillis() - start;
 //        System.out.printf("Total time: %s ms", totalTime); // Total time: 0 ms
 
-        SequencedSet<Integer> numbers = new LinkedHashSet<>();
-        numbers.addFirst(1);
-        numbers.addLast(2); // аналогично методу add()
-        numbers.addLast(3);
-        numbers.addFirst(1); // игнорируем, т.к. уже добавлен
+//        SequencedSet<Integer> numbers = new LinkedHashSet<>();
+//        numbers.addFirst(1);
+//        numbers.addLast(2); // аналогично методу add()
+//        numbers.addLast(3);
+//        numbers.addFirst(1); // игнорируем, т.к. уже добавлен
+//
+//        System.out.println(numbers); // [1, 2, 3]
+//        System.out.println(numbers.reversed()); // [3, 2, 1]
+//
+//        numbers.removeFirst();
+//        numbers.removeLast();
+//        System.out.println(numbers); // [2]
+
+
+        SequencedSet<Integer> numbers = new TreeSet<>();
+        numbers.add(2); // addfirst() and addLast() не поддерживаются, т.к. TreeSet сортирует элементы и нельзя определить порядок при добавлении
+        numbers.add(3);
+        numbers.add(1);
 
         System.out.println(numbers); // [1, 2, 3]
-        System.out.println(numbers.reversed()); // [3, 2, 1]
-
-        numbers.removeFirst();
-        numbers.removeLast();
-        System.out.println(numbers); // [2]
-
 
     }
 }
